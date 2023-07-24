a5fdc83ed3a3ca43cfab7bfeda22609db45a8f41
Phonratichai <phonratichai.dev@gmail.com>
2023-07-25 00:44:23 +0700

    fix: update image

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function App(): ReactElement {
               </span>
             </div>
             <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4" >
-              <img width={1500} height={1000} src={DemoBG} />
+              <img width={1080} height={720} src={DemoBG} />
             </div>
           </div>
         </div>
